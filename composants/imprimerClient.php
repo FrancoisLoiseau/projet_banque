9c3583720414ac7f6144f1b324a208a47f0908ee
Christophe Guidé <guide.christophe@live.fr>
2021-10-04 13:24:20 +0200

    rework imprimerClient

--- composants/imprimerClient.php
+++ composants/imprimerClient.php
@@ -9,18 +9,15 @@ function imprimerClient(){
 
     if(($fichier = fopen("./bdd/client.csv", "r")) !== FALSE) {
         while (($data = fgetcsv($fichier, 1000, ";")) !== FALSE) {
-            $taille = count($data);
-            for ($i=0; $i < $taille; $i++) {
-                if($data[$i] == $recherche){
-                    $client = [
-                        "id_client" => $data[0],
-                        "nom" => $data[1],
-                        "prenom" => $data[2],
-                        "date_naissance" => $data[3],
-                        "email" => $data[4],
-                    ];
-                    break;
-                }
+            if($data[$i] == $recherche){
+                $client = [
+                    "id_client" => $data[0],
+                    "nom" => $data[1],
+                    "prenom" => $data[2],
+                    "date_naissance" => $data[3],
+                    "email" => $data[4],
+                ];
+                break;
             }
         }
         fclose($fichier);
@@ -32,19 +29,16 @@ function imprimerClient(){
     else{
         if (($fichier = fopen("./bdd/compte.csv", "r")) !== FALSE) {
             while (($data = fgetcsv($fichier, 1000, ";")) !== FALSE) {
-                $taille = count($data);
-                for ($i=0; $i < $taille; $i++) {
-                    if($data[$i] == $client["id_client"]){
-                        $compte = [
-                            "id_compte" => $data[0],
-                            "id_agence" => $data[1],
-                            "id_client" => $data[2],
-                            "solde" => $data[3],
-                            "decouvert_autorise" => $data[4],
-                        ];
-                        $comptes[] = $compte;
-                        break;
-                    }
+                if($data[$i] == $client["id_client"]){
+                    $compte = [
+                        "id_compte" => $data[0],
+                        "id_agence" => $data[1],
+                        "id_client" => $data[2],
+                        "solde" => $data[3],
+                        "decouvert_autorise" => $data[4],
+                    ];
+                    $comptes[] = $compte;
+                    break;
                 }
             }
             fclose($fichier);
@@ -65,7 +59,7 @@ function imprimerClient(){
             echo("------------------------------------------------------\n");
             echo("Numéro de compte\tSolde\t\n");
             echo("------------------------------------------------------\n");
-            foreach($comptes as$compte){
+            foreach($comptes as $compte){
                 $smiley = "";
                 if($compte["solde"] > 0){
                     $smiley = ":-)";

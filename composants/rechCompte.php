7add6857b441f1aaac77c47424a39cb4ae4f4cf9
FrancoisLoiseau <francoisloiseau62@gmail.com>
2021-10-06 09:49:12 +0200

    bugfix rechCompte

--- composants/rechCompte.php
+++ composants/rechCompte.php
@@ -9,26 +9,24 @@ $listeCompte3=null;
 
 if (($fichier=fopen("./bdd/compte.csv","r"))!==false) {
     while(($data=fgetcsv($fichier,1000,";"))!==false) {
+        $i=1;
         if($data[0]==$num_compte) {
-            $i=1;
-            $compte=[$data[1],$data[2],$data[3],$data[4],$data[5],
-              
-                    ];
-                    if ($i=1) {
-                        $listCompte1[]=$compte;
-                    }
-                    if ($i=1) {
-                        $listCompte2[]=$compte;
-                    }
-                    if ($i=3) {
-                        $listCompt31[]=$compte;
-                    }
-                    
-                    if ($i=3) {
-                        break;
-                    }
+            $compte=[$data[1],$data[2],$data[3],$data[4],$data[5]];
+            if ($i==1) {
+                $listeCompte1=$compte;
+            }
+            if ($i==2) {
+                $listeCompte2=$compte;
+            }
+            if ($i==3) {
+                $listeCompte3=$compte;
+            }
+            
+            if ($i==3) {
+                break;
+            }
 
-                    $i++;
+            $i++;
 
         }
     }
